25e30484324ecdfc9e40542f40753e8b86004050
Nengzhuo Cai <null@203.io>
2024-09-11 02:32:12 -0400

    Initial Changes

---
+++ docusaurus.config.ts
@@ -0,0 +1,164 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: '203 Systems',
+  favicon: 'img/favicon.png',
+
+  // Set the production url of your site here
+  url: 'https://wiki.203.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: '203-Systems', // Usually your GitHub org/user name.
+  projectName: 'Matrix-Wiki', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh-Hans'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/203-Systems/Matrix-Wiki/tree/main/',
+        },
+        blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/203-Systems/Matrix-Wiki/tree/main/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      // title: '203 Systems',
+      logo: {
+        alt: '203 Systems',
+        src: 'img/203.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'mystrixSidebar',
+          position: 'left',
+          label: 'Mystrix',
+        },
+        {
+          type: 'docSidebar',
+          sidebarId: 'matrixOsSidebar',
+          position: 'left',
+          label: 'MatrixOS',
+        },
+        // {to: '/blog', label: 'Updates', position: 'left'},
+        {
+          type: 'docsVersionDropdown',
+          position: 'right',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/203-Systems',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Mystrix',
+              to: '/docs/Mystrix',
+            },
+            {
+              label: 'Matrix OS',
+              to: '/docs/MatrixOS',
+            }
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/rRVCBHHPfw',
+            },
+            {
+              label: 'Twitter / X',
+              href: 'https://twitter.com/203Systems',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://instagram.com/203null',
+            },
+            {
+              label: 'Youtube',
+              href: 'https://www.youtube.com/channel/UCl9tL-r4NzvGl-EIeVYZZ-g',
+            },
+            {
+              label: 'Facebook',
+              href: 'https://facebook.com/203Systems',
+            }
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: '203 Systems',
+              to: 'https://203.io',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/203-Systems',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} 203 Systems`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Heading from '@theme/Heading';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          Just Got Your Mystrix?
+        </Heading>
+        <p className="hero__subtitle">Get Ready to Dive In!</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/Mystrix/GettingStarted">
+            Getting Started with Mystrix
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Project Matrix Wiki`}
+      description="Wiki and Documentations of everything 203 Systems">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
