6f0dfa11bc1f977246267849516d0747809553d7
Nengzhuo Cai <null@203.io>
2024-12-14 08:15:37 -0500

    Animated gradient

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,33 +22,12 @@ export default function Home(): JSX.Element {
       title={`Project Matrix Wiki`}
       description="Wiki and Documentations of everything 203 Systems">
       <main>
-      <div style={{ position: 'relative', height: '0vh' }}>
-            {/* Background using ::before pseudo-element */}
-          <div style={{
-              position: 'absolute',
-              top: '5vh',
-              left: '5vh',
-              width: '90vh',
-              height: '90vh',
-              zIndex: -1, // Ensure background is behind content
-              backgroundColor: '#ff99df',
-              backgroundImage: `
-                  radial-gradient(circle at 52% 73%, hsla(310, 85%, 67%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 0% 30%, hsla(197, 90%, 76%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 41% 26%, hsla(234, 79%, 69%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 41% 51%, hsla(41, 70%, 63%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 41% 88%, hsla(36, 83%, 61%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 76% 73%, hsla(346, 69%, 70%, 1) 0px, transparent 50%),
-                  radial-gradient(circle at 29% 37%, hsla(272, 96%, 64%, 1) 0px, transparent 50%)`,
-              backgroundSize: '150% 150%',
-              filter: 'blur(80px)',
-              animation: 'moveBackground 10s linear infinite',
-          }}></div>
-
-      </div>
+      <div className={styles.background_container}>
+      <div className={styles.background}></div>
+  </div>
       <div className={styles.main_container}>
         <Heading as="h1" className={styles.hero_title}>
-          Project Matrix Wiki
+          Project&nbsp;&#8203;Matrix Wiki
         </Heading>
         <p className={styles.hero_subtitle}>
           Get Started With your Mystrix
